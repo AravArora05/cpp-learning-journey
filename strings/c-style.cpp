87bc71347c07c7eb5500d424ab90d378a342274c
AravArora05 <81041030+AravArora05@users.noreply.github.com>
2025-12-30 16:08:13 -0500

    Create c-style.cpp

---
+++ strings/c-style.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void strings_and_functions() {
+    
+    char first_name[] {"Bjarne"};
+    char last_name[] {"Stroustrup"};
+
+    size_t first_name_length {strlen(first_name)};
+    size_t last_name_length {strlen(last_name)};
+
+    char whole_name[50];  
+
+    strcpy(whole_name, first_name);
+    strcat(whole_name, last_name);
+
+    size_t whole_name_length {strlen(whole_name)};
+    
+
+    cout << "The length of the first name, " << first_name
+         << ", is " << first_name_length
+         << " letters long and the length of the last name, "
+         << last_name << ", is " << last_name_length
+         << " letters long. This means that the length of the whole name must be "
+         << whole_name_length << " letters long.";
+}
