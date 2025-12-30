530b6014bfe642ee5b792ade25937ec50174531a
AravArora05 <81041030+AravArora05@users.noreply.github.com>
2025-12-30 16:50:43 -0500

    Create Class.h

---
+++ OOP/Class.h
@@ -0,0 +1,29 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+#include <string>
+using namespace std;
+
+class Dog {
+private:
+    string name;
+    int age;
+public:
+
+string get_name() {
+    return name;
+}
+
+int get_age() {
+    return age;
+}
+
+void set_name(string phrase) {
+    name = phrase;
+}
+
+void set_age(int number) {
+    age = number;
+}
+
+};
+#endif 
